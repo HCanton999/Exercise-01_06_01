ba4c6011bae1a999d048bf7e9f3ec5b1d7f3d8ea
Unknown <hcanton999@west-mec.org>
2018-08-06 15:29:21 -0700

    Getting rid of system defaults
    made a new javascript file and began removing system defaults by using functions and if elses

--- snoot.js
+++ snoot.js
@@ -5,9 +5,20 @@ form validation code for snoot.html
 Author:Harrison Canton
 Date:8.6.18
 
-Snoot Flowers order form
-Author: Harrison Canton
-Date:   8.6.18
-
-Filename: snoot.html
+Filename: snoot.js
 */
+
+"use strict";
+
+// a function to remove select list defaults
+function removeSelectDefaults() {
+    var emptyBoxes = document.getelementsByTagName("select");
+    alert("select lists: " + emptyBoxes.length);
+}
+
+// Page load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", removeSelectDefaults, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", removeSelectDefaults);
+}
