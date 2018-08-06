eb6fd2657cbaecef7861d432d0ea293720e755ab
Unknown <hcanton999@west-mec.org>
2018-08-06 14:30:13 -0700

    Initial push x2

---
+++ snoot.js
@@ -0,0 +1,13 @@
+
+/*
+Snoot.js
+form validation code for snoot.html
+Author:Harrison Canton
+Date:8.6.18
+
+Snoot Flowers order form
+Author: Harrison Canton
+Date:   8.6.18
+
+Filename: snoot.html
+*/
