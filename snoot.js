bc53e003eab1fb77fb68b16238334250c9cfa22d
Unknown <hcanton999@west-mec.org>
2018-08-07 15:21:18 -0700

    Fix day/month issues
    So some of the months have incorrect days like February so we began to attempt to fix that by creating multiple functions. we changed some things we did previously by creating more functions.

--- snoot.js
+++ snoot.js
@@ -10,15 +10,51 @@ Filename: snoot.js
 
 "use strict";
 
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // a function to remove select list defaults
 function removeSelectDefaults() {
-    var emptyBoxes = document.getelementsByTagName("select");
-    alert("select lists: " + emptyBoxes.length);
+    var emptyBoxes = document.getElementsByTagName("select");
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+// function to set up document fragments for days of the month
+function setUpDays() {
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+//function to set up the list of days based the selected month
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+}
+
+// function that sets up page on load event
+function setUpPage() {
+    removeSelectDefaults();
+    setUpDays();
+    updateDays();
 }
 
 // Page load event handlers
 if (window.addEventListener) {
-    window.addEventListener("load", removeSelectDefaults, false);
+    window.addEventListener("load", setUpPage, false);
 } else if (window.attachEvent) {
-    window.attachEvent("onload", removeSelectDefaults);
+    window.attachEvent("onload", setUpPage);
 }
