c566f611aa03e026de91c21227432f118f492337
Unknown <hcanton999@west-mec.org>
2018-08-09 15:27:39 -0700

    Added copying features
    Used functions to copy billing address information to also the delivery address.  We used a couple for loops so that we can make sure when we unclick the check box it will undue what some of the copying.

--- snoot.js
+++ snoot.js
@@ -60,6 +60,43 @@ function updateDays() {
     }
 }
 
+//function to inspect custom check box on message change
+function autoCheckCustom() {
+    var messageBox = document.getElementById("customText");
+    // textarea has message, check the box
+    if (messageBox.value !== "" && messageBox.value !== messageBox.placeholder) {
+        document.getElementById("custom").checked = "checked";
+    }
+    //textarea has no message, uncheck the box
+    else {
+        document.getElementById("custom").checked = "";
+    }
+}
+
+//function to cop billing to delivery address
+    function copyBillingAddress() {
+        var billingInputElements = document.querySelectorAll("#billingAddress input");
+        var deliveryInputElements = document.querySelectorAll("#deliveryAddress input");
+        // duplicate address - checkbox is checked - copy
+        if (document.getElementById("sameAddr").checked) {
+            for (var i = 0; i < billingInputElements.length; i++){
+                deliveryInputElements[i + 1].value =
+                billingInputElements[i].value;
+            }
+            document.querySelector("#deliveryAddress select").value =
+            document.querySelector("#billingAddress select").value;
+        }
+        // duplicate address - checkbox not checked - erase  
+        else {
+            for (var i = 0; i < billingInputElements.length; i++){
+                deliveryInputElements[i + 1].value = "";
+            }
+            document.querySelector("#deliveryAddress select").selectedIndex = -1;
+            
+        }
+        }  
+    
+
 // function that sets up page on load event
     function setUpPage() {
     removeSelectDefaults();
@@ -80,6 +117,18 @@ function updateDays() {
             deliveryYear.addEventListener("change", updateDays, false);
         } else if (deliveryYear.attachEvent) {
             deliveryYear.attachEvent("onchange", updateDays);  
+        }
+        var messageBox = document.getElementById("customText");
+        if (messageBox.addEventListener) {
+            messageBox.addEventListener("change", autoCheckCustom, false);
+        } else if (messageBox.attachEvent) {
+            messageBox.attachEvent("onchange", autoCheckCustom);  
+        }
+        var same = document.getElementById("sameAddr");
+        if (same.addEventListener) {
+            same.addEventListener("change", copyBillingAddress, false);
+        } else if (same.attachEvent) {
+            same.attachEvent("onchange", copyBillingAddress);  
         }
     }
 // Page load event handlers
