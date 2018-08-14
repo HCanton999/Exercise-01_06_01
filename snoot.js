de7b0aa42380daaabce4cedf2296058688b0d092
Unknown <hcanton999@west-mec.org>
2018-08-14 15:27:51 -0700

    We added more validation
    WE created more functions for more of the form to be validated and work properly. Today was a debug day so not the most got done but we created validation functions for delivery address and payment.

--- snoot.js
+++ snoot.js
@@ -40,6 +40,10 @@ function updateDays() {
     var dates = deliveryDay.getElementsByTagName("option");
     var deliveryMonth = document.getElementById("delivMo");
     var deliveryYear = document.getElementById("delivYr");
+    // cover for no month selected
+    if (deliveryMonth.selectedIndex === -1) {
+        return;
+    }
     var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
     while (dates[28]) {
         deliveryDay.removeChild(dates[28]);
@@ -146,6 +150,81 @@ function autoCheckCustom() {
             }
         }
 
+        // function to validate delivery date
+        function validateDeliveryAddress() {
+            var selectElements = document.querySelectorAll("#deliveryDate" + " select");
+            var errorDiv = document.querySelectorAll("#deliveryDate" + " .errorMessage")[0];
+            var fieldsetValidity = true;
+            var elementCount = selectElements.length;
+            var currentElement;
+            try {
+                //loop through the select fields looking for blanks 
+                for (var i = 0; i < elementCount; i++) {
+                    currentElement = selectElements[i];
+                    //blanks
+                    if (currentElement.selectedIndex === -1) {
+                        currentElement.style.border = "1px solid red";
+                        fieldsetValidity = false;       
+                    }
+                    //not blanks
+                    else {
+                        currentElement.style.border = "";
+ 
+                    }
+                }
+                // action for invalid fieldset
+                if (!fieldsetValidity) {
+                        throw "please specify a Delivery Date";
+                }else {
+                    errorDiv.style.display = "none";
+                    errorDiv.innerHTML = "";
+                }
+            } catch (msg){
+            errorDiv.style.display = "block";
+            errorDiv.innerHTML = msg;
+            formValidity = false;
+            }
+        }
+        // function to validate payment
+        function validatepayment() {
+            var selectElements = document.querySelectorAll("#paymentInfo" + " select");
+            var ccNumElement = document.getElementById("ccNum")
+            var errorDiv = document.querySelectorAll("#paymentInfo" + " .errorMessage")[0];
+            var fieldsetValidity = true;
+            var elementCount = selectElements.length;
+            var cvvElement = document.getElementById("cvv");
+            var cards = document.getElementsByName("PaymentType");
+            var currentElement;
+            //THIS IS WHERE WE WERE YESTURDAY
+            try {
+                //loop through the select fields looking for blanks 
+                for (var i = 0; i < elementCount; i++) {
+                    currentElement = selectElements[i];
+                    //blanks
+                    if (currentElement.selectedIndex === -1) {
+                        currentElement.style.border = "1px solid red";
+                        fieldsetValidity = false;       
+                    }
+                    //not blanks
+                    else {
+                        currentElement.style.border = "";
+ 
+                    }
+                }
+                // action for invalid fieldset
+                if (!fieldsetValidity) {
+                        throw "please specify a Delivery Date";
+                }else {
+                    errorDiv.style.display = "none";
+                    errorDiv.innerHTML = "";
+                }
+            } catch (msg){
+            errorDiv.style.display = "block";
+            errorDiv.innerHTML = msg;
+            formValidity = false;
+            }
+        }
+
         //function to validate entire form
     function validateForm(evt) {
         if (evt.preventDefault) {
@@ -156,6 +235,7 @@ function autoCheckCustom() {
         formValidity = false;
         validateAddress('billingAddress');
         validateAddress('deliveryAddress');
+        validateDeliveryAddress();
         if (formValidity === true) {//form is valid
             document.getElementById("errorText").innerHTML = "";
             document.getElementById("errorText").style.display = "none";
@@ -172,6 +252,7 @@ function autoCheckCustom() {
     removeSelectDefaults();
     setUpDays();
     createEventListeners();
+    
 }
 
 //function to create our event listeners
